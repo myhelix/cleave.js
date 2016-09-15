8e36535ae5f1017386ceebe52a5c62b30f50af0d
Toby Berster <toby.berster@helix.com>
2016-09-15 14:01:02 -0700

    proper alphanumeric filter

--- src/Cleave.js
+++ src/Cleave.js
@@ -200,7 +200,7 @@ Cleave.prototype = {
         value = pps.numericOnly ? Util.strip(value, /[^\d]/g) : value;
 
         // strip non-alphanumeric characters
-        value = pps.alphaNumericOnly ? Util.strip(value, /[^\W]/g) : value;
+        value = pps.alphaNumericOnly ? Util.strip(value, /[^\A-Za-z0-9]/g) : value;
 
         // convert case
         value = pps.uppercase ? value.toUpperCase() : value;
